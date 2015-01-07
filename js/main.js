55b2681ffa5fe3cae5088b6c4268f9ffc6bbe66a
William Boman <william@redwill.se>
2015-01-07 01:41:20 +0100

    Enclose script in IIFE. Now sets URL hash when playing a video. Automatically plays eventual video specified in hash when loading the page.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,64 @@
-'use strict';
+(function (window, jQuery) {
+  'use strict';
 
-var tag = document.createElement('script');
-tag.src = "//www.youtube.com/iframe_api";
+  var tag = document.createElement('script');
+  tag.src = "//www.youtube.com/iframe_api";
 
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
+  var player;
+  window.onYouTubeIframeAPIReady = function() {
+    player = new YT.Player('player', {
+      height: '585',
+      width: '960',
+      videoId: 'a1Y73sPHKxw',
+      playerVars: {
+        loop: '1'
+      },
+      events: {
+        onReady: onReady
+      }
+    });
 
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '585',
-    width: '960',
-    videoId: 'a1Y73sPHKxw',
-    playerVars: {
-      loop: '1'
+    function onReady() {
+      var playVideo;
+
+      // Add listeners and load eventual video from hash when player is ready.
+      jQuery(document).ready(function($) {
+        var regex = /^\s?(https?:\/\/)?(www.)?(youtube.com)?(\/watch\?v=)?([a-zA-Z0-9_-]{11})\s?$/;
+        var $url = $('#url');
+        var $invalid = $('#invalid');
+        
+        playVideo = function (video_id) {
+          $invalid.hide();
+          var val = video_id || $url.val();
+          var res = regex.exec(val);
+          if (res) {
+            var videoId = res[5];
+            player.loadVideoById(videoId);
+            window.location.hash = videoId;
+          } else {
+            // Invalid value
+            $invalid.find('span').text(val);
+            $invalid.show();
+          }
+        }
+
+        $('#videoId').on('submit', function(e) {
+          e.preventDefault();
+          playVideo();
+          return false;
+        });
+
+        if (window.location.hash) {
+          playVideo(window.location.hash.slice(1));
+        }
+      });
+
+      $(window).on('hashchange', function () {
+        playVideo(window.location.hash.slice(1));
+      });
     }
-  });
-}
-
-jQuery(document).ready(function($) {
-
-  var regex = /^\s?(https?:\/\/)?(www.)?(youtube.com)?(\/watch\?v=)?([a-zA-Z0-9_-]{11})\s?$/;
-  var $url = $('#url');
-  var $invalid = $('#invalid');
-
-  $('#videoId').on('submit', function(e) {
-    e.preventDefault();
-    $invalid.hide();
-
-    var val = $url.val();
-    var res = regex.exec(val);
-    if (res) {
-      var videoId = res[5];
-      player.loadVideoById(videoId);
-    } else {
-      // Invalid value
-      $invalid.find('span').text(val);
-      $invalid.show();
-    }
-    return false;
-  });
-
-});
+  }
+})(window, jQuery);
